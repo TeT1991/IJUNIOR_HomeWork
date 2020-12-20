64ea1054ce15fdd13bbbff3dedda1b4f50c978d4
TeT91 <antrab1991@gmail.com>
2020-12-20 11:56:57 +0300

    first commit

---
+++ TrashBin/Trashbin/Assets/Scripts/Trash.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent(out Bin bin)) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
