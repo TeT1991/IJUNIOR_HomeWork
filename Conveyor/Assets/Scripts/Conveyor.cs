59fbc694d473f26fa7e67e5ea9864f3b8e9dbea9
TeT91 <antrab1991@gmail.com>
2020-12-20 18:48:30 +0300

    git push!

---
+++ Conveyor/Assets/Scripts/Conveyor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public float GetSpeed()
+    {
+        return _speed;
+    }
+}
---
+++ Conveyor/Assets/Scripts/Cube.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody2D;
+    private float _speed;
+
+    private void Start()
+    {
+        _speed = 0;
+        _rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Conveyor>(out Conveyor conveyor))
+        {
+            _speed = collision.gameObject.GetComponent<Conveyor>().GetSpeed();
+        }
+    }
+
+    private void Update()
+    {
+        _rigidBody2D.AddForce(Vector2.right * _speed);
+    }
+}
