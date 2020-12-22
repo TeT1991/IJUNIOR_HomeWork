ac8bc393487357621839a2a0b87b298897f2a997
TeT91 <antrab1991@gmail.com>
2020-12-22 16:13:08 +0300

    111

--- TrashBin/Trashbin/Assets/Scripts/Spawner.cs
+++ TrashBin/Trashbin/Assets/Scripts/Spawner.cs
@@ -5,18 +5,13 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject _trash;
-
     private Vector3 _mousePosition;
-
-
-
 
     private void Update()
     {
-        _mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-
         if (Input.GetMouseButtonDown(0))
         {
+            _mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
             Instantiate(_trash, _mousePosition, Quaternion.identity);
         }
     }
