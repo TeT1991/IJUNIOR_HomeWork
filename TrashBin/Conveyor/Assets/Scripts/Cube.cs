a61e6476b3fb3ce9695e7bbc29414acdd0c4b854
TeT91 <antrab1991@gmail.com>
2020-12-22 16:45:37 +0300

    111111111

---
+++ TrashBin/Conveyor/Assets/Scripts/Cube.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody2D;
+    private float _speed;
+
+    private void Start()
+    {
+        _speed = 0;
+        _rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Conveyor>(out Conveyor conveyor))
+        {
+            SetSpeed(collision.gameObject.GetComponent<Conveyor>().GetSpeed());
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Conveyor>(out Conveyor conveyor))
+        {
+            SetSpeed(0);
+        }
+    }
+
+    private void Update()
+    {
+        _rigidBody2D.AddForce(Vector2.right * _speed);
+    }
+
+    private void SetSpeed(float speed)
+    {
+        _speed = speed;
+    }
+}
---
+++ TrashBin/Conveyor/Assets/Scripts/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _cube;
+
+    private Vector3 _mousePosition;
+
+    private void Update()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(_cube, _mousePosition, Quaternion.identity);
+        }
+    }
+}
